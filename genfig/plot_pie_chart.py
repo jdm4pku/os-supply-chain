c16760fe62ed4a53bf9f1e236b5aabec725c4af0
jdm4pku <jdmpku@gmail.com>
2024-04-27 19:18:34 +0800

    add pie chart

---
+++ genfig/plot_pie_chart.py
@@ -0,0 +1,11 @@
+
+
+
+
+if __name__=="__main__":
+    os_versions = [
+        ("fedora", "x86_64", "38"),
+        ('centos', 'x86_64', '7'),
+        ("openEuler", "x86_64", "openEuler-23.09"),
+    ]
+    plot_pie(os_version)
